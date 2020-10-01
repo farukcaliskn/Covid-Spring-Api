4a531296180b1c37c27ec4fd5396f2c4eeebb14f
farukcaliskan <faruk.caliskn@outlook.com>
2020-10-02 01:41:50 +0300

    initial commit.

---
+++ src/main/java/com/covid/spring/Application.java
@@ -0,0 +1,35 @@
+package com.covid.spring;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+
+@SpringBootApplication
+public class Application {
+    static ArrayList<String> value = new ArrayList<>();
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+        Document doc = null;
+        try {
+            doc = Jsoup.connect("https://www.worldometers.info/coronavirus/").get();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        for (int i = 0; i < 3; i++) {
+            if (doc != null) {
+                value.add(doc.select("div.maincounter-number").get(i).select("span").text());
+            }
+        }
+
+
+        System.out.println(value.get(1));
+    }
+
+}
