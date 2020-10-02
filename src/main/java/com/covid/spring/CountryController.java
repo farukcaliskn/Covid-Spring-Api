356048980fdb1469204c8efb342b78058b29928a
Your Name <emreesen27@gmail.com>
2020-10-02 16:20:48 +0300

    minor corrections done.

--- src/main/java/com/covid/spring/CountryController.java
+++ src/main/java/com/covid/spring/CountryController.java
@@ -7,10 +7,11 @@ import org.springframework.web.bind.annotation.*;
 
 @Controller
 class CountryController {
+    JsoupParser parser = JsoupParser.getInstance();
+
     @RequestMapping(value = "cases", method = RequestMethod.GET)
     public @ResponseBody
     Country getItem(@RequestParam("country") String country) {
-        JsoupParser parser = JsoupParser.getInstance();
         String cases = parser.getCountry(country).get(0);
         String deaths = parser.getCountry(country).get(1);
         String recovered = parser.getCountry(country).get(2);
